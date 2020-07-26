1f7d652a6a8e47509b7887f70a0fd688bc03116c
abhinavvarshney <abhinav.varshney@geminisolutions.in>
2020-07-26 17:34:29 +0530

    Code Documentation in Controller

--- src/main/java/com/gemini/assignmentjavagroupfive/AssignmentJavaGroupFiveApplication.java
+++ src/main/java/com/gemini/assignmentjavagroupfive/AssignmentJavaGroupFiveApplication.java
@@ -6,6 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AssignmentJavaGroupFiveApplication {
 
+	/**
+	 *
+	 * This spring boot application implements CRUD operations
+	 * on a filesystem
+	 *
+	 * @author Abhinav Varshney
+	 * @version 1.0
+	 *
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentJavaGroupFiveApplication.class, args);
 	}
--- src/main/java/com/gemini/assignmentjavagroupfive/controller/FileRestController.java
+++ src/main/java/com/gemini/assignmentjavagroupfive/controller/FileRestController.java
@@ -4,8 +4,11 @@ package com.gemini.assignmentjavagroupfive.controller;
 import com.gemini.assignmentjavagroupfive.dataModel.FileEntity;
 import com.gemini.assignmentjavagroupfive.service.FilesService;
 import com.gemini.assignmentjavagroupfive.exception.RecordNotFoundException;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,13 @@ import java.io.IOException;
 import java.lang.management.OperatingSystemMXBean;
 import java.util.List;
 
+/**
+ * REST API Controller Class
+ * Implementing all CRUD APIs
+ *
+ * @BasePath localhost:8080/file
+ * */
+
 @RestController
 @RequestMapping("/file")
 public class FileRestController {
@@ -29,15 +39,27 @@ public class FileRestController {
 
 
     @Operation(summary = "Get all files",description = "REST Endpoint to get all the files")
-    @GetMapping(value = "/")
+    @ApiResponse()
+    @GetMapping(value = "/",produces = "application/json")
     public ResponseEntity<List<FileEntity>> getAllFile() {
+        /**
+         *GET API method to get all the files in a directory
+         * @apiNote localhost:8080/file/
+         * @response JSON object containing id,fileName,fileContent,filePath
+         * */
         List<FileEntity> list = filesService.getAllFiles();
         return new ResponseEntity<List<FileEntity>>(list, new HttpHeaders(), HttpStatus.OK);
     }
 
     @Operation(summary = "Get File by its name",description = "REST Endpoint to get a file by its name")
-    @GetMapping("/{fileName}")
+    @GetMapping(value = "/{fileName}")
     public ResponseEntity<Object> getFileByName(@PathVariable("fileName") String fileName) throws RecordNotFoundException {
+        /**
+         * GET API BY FILE NAME method to get file by file name
+         * @apiNote localhost:8080/file/{fileName}
+         * @response JSON object containing id,fileName,fileContent,filePath
+         *  */
+
         FileEntity entity = filesService.getFileByName(fileName);
         return new ResponseEntity<>(entity.getFileContent(),new HttpHeaders(),HttpStatus.OK);
     }
@@ -45,22 +67,41 @@ public class FileRestController {
     @Parameter(name = "fileName",description = "Name of the file",required = true)
     @Parameter(name = "fileContent",description = "Content of the file",required = true)
     @Operation(summary = "Create a new File",description ="REST Endpoint that returns a object after creating a file")
-    @PostMapping
+    @PostMapping(value = "/")
     public ResponseEntity<Object> createFile(@RequestBody FileEntity fileEntity) throws IOException {
+        /**
+         * POST API method to create file
+         * @apiNote localhost:8080/file/
+         * @param args1 fileName
+         * @param args2 fileContent
+         * @response status message
+         *  */
         FileEntity entity=filesService.createFile(fileEntity);
         return new ResponseEntity<Object>("File created successfully",new HttpHeaders(),HttpStatus.CREATED);
     }
 
     @Operation(summary = "Update a content of file by its name",description = "REST Endpoint to update a file by its name")
-    @PutMapping("/{fileName}")
+    @PutMapping(value = "/{fileName}",produces = "application/json")
     public ResponseEntity<FileEntity> updateFile(@PathVariable("fileName")String fileName,@RequestBody FileEntity fileEntity) throws RecordNotFoundException, IOException {
+        /**
+         * PUT API BY FILE NAME method to update the content of a file
+         * @apiNote localhost:8080/file/{fileName}
+         * @param args1 fileName
+         * @param args2 fileContent
+         * @response JSON object containing id,fileName,fileContent,filePath
+         * */
         FileEntity entity=filesService.updateFile(fileName,fileEntity);
         return new ResponseEntity<FileEntity>(entity,new HttpHeaders(),HttpStatus.OK);
     }
 
     @Operation(summary = "Delete a file by its name",description = "REST Endpoint to delete a file by its name")
-    @DeleteMapping("/{fileName}")
+    @DeleteMapping(value = "/{fileName}")
     public HttpStatus deleteFileByName(@PathVariable("fileName")String fileName) throws RecordNotFoundException{
+        /**
+         * DELETE API BY FILE NAME method to delete a file
+         * @apiNote localhost:8080/file/{fileName}
+         * @response status message
+         * */
         filesService.deleteFileByName(fileName);
         return HttpStatus.FORBIDDEN;
     }
